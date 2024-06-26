5eb92dc206b878a995631d0110dfaac879febb18
sona30k <sona22kerketta@gmail.com>
2024-06-26 17:04:19 +0530

    Initial Commit

---
+++ Finance Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance Manager
+//
+//  Created by Sona kerketta on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance Manager/Finance_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_ManagerApp.swift
+//  Finance Manager
+//
+//  Created by Sona kerketta on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Finance_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
