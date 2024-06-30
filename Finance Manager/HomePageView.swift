8d895b6e30062b96232558201a03a1a2701029a6
sona30k <sona22kerketta@gmail.com>
2024-06-30 12:03:34 +0530

    HomePage

---
+++ Finance Manager/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  Finance Manager
+//
+//  Created by Sona kerketta on 29/06/24.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
