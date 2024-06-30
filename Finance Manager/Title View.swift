958cf6db3525ec14b61140b1c0172d7bdaed706f
sona30k <sona22kerketta@gmail.com>
2024-06-30 17:05:11 +0530

    Main

---
+++ Finance Manager/Title View.swift
@@ -0,0 +1,18 @@
+//
+//  Title View.swift
+//  Finance Manager
+//
+//  Created by Sona kerketta on 30/06/24.
+//
+
+import SwiftUI
+
+struct Title_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Title_View()
+}
